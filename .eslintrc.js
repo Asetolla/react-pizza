79c01f9fe519b4057b3b6a642f689b889cc8dd9f
Aset Bagdat <aset10bagdat@gmail.com>
2026-04-20 18:01:21 +0500

    trying to fix dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  root: true,
+
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+    es2021: true,
+  },
+
+  extends: [
+    "react-app",
+    "react-app/jest",
+    "plugin:storybook/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+
+  plugins: ["react", "@typescript-eslint"],
+
+  parser: "@typescript-eslint/parser",
+
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
+
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+
+  rules: {
+    // TS
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-explicit-any": "off",
+
+    // React
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+
+    // General
+    "no-console": "warn",
+    "no-empty": "warn",
+    "no-unused-expressions": "error",
+  },
+};
