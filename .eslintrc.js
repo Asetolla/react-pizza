487d8dc66839b93d85ce1cead8165754205a5ccb
Aset Bagdat <aset10bagdat@gmail.com>
2026-05-02 23:06:06 +0500

    Building project

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,39 +9,47 @@ module.exports = {
   },
 
   extends: [
-    "react-app",
-    "react-app/jest",
-    "plugin:storybook/recommended",
-    "prettier",
+    'react-app',
+    'react-app/jest',
+    'plugin:storybook/recommended',
+    'prettier',
   ],
 
-  plugins: ["react", "@typescript-eslint"],
+  plugins: ['react', '@typescript-eslint'],
 
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
 
   parserOptions: {
     ecmaVersion: 2021,
-    sourceType: "module",
+    sourceType: 'module',
   },
 
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
 
   rules: {
     // TS
-    "@typescript-eslint/no-unused-vars": "warn",
-    "@typescript-eslint/no-explicit-any": "off",
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
 
     // React
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
 
     // General
-    "no-console": "warn",
-    "no-empty": "warn",
-    "no-unused-expressions": "error",
+    'no-console': 'warn',
+    'no-empty': 'warn',
+    'no-unused-expressions': 'error',
   },
-};
+  overrides: [
+    {
+      files: ['*.test.ts'],
+      rules: {
+        'no-console': 'off',
+      },
+    },
+  ],
+}
